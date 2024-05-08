c56f52590d0c55de3324fd0242393f31c76976d6
Amar Gandhi <amargandhi10@gmail.com>
2024-05-07 20:21:55 -0700

    Working file upload/save to disk

--- client.go
+++ client.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"html"
-
 	"github.com/gorilla/websocket"
+	"html"
+	"log"
+	"os"
 )
 
 // client represents a single chatting user.
@@ -29,34 +31,63 @@ type clientMessage struct {
 	Body   string
 }
 
+type fileMetadata struct {
+	Name string
+	Type string
+}
+
 // received message from this client
 func (c *client) read() {
 	defer c.socket.Close()
 	for {
-		_, msg, recvErr := c.socket.ReadMessage()
+		messageType, msg, recvErr := c.socket.ReadMessage()
 		if recvErr != nil {
 			return
 		}
 
-		fmt.Printf("%s\n", msg)
+		if messageType == websocket.TextMessage {
+			fmt.Printf("%s\n", msg)
 
-		var parsed clientMessage
-		parseErr := json.Unmarshal(msg, &parsed)
-		if parseErr != nil {
-			fmt.Printf("Message parse error: %s\n", msg)
-			// if parseErr we can just ignore the message
-		} else {
-			fmt.Printf("%s %s\n", parsed.Method, parsed.Body)
-			switch parsed.Method {
-			case "name":
-				c.name = parsed.Body
-			case "message":
-				escaped := html.EscapeString(parsed.Body)
-				encodedMessage, _ := json.Marshal(map[string]string{"sender": c.name, "message": escaped})
-				c.room.forward <- encodedMessage
+			var parsed clientMessage
+			parseErr := json.Unmarshal(msg, &parsed)
+			if parseErr != nil {
+				fmt.Printf("Message parse error: \n")
+			} else {
+				fmt.Printf("%s %s\n", parsed.Method, parsed.Body)
+				switch parsed.Method {
+				case "name":
+					c.name = parsed.Body
+				case "message":
+					escaped := html.EscapeString(parsed.Body)
+					encodedMessage, _ := json.Marshal(map[string]string{"sender": c.name, "message": escaped})
+					c.room.forward <- encodedMessage
+				}
+			}
+		} else if messageType == websocket.BinaryMessage {
+			metadataEndIdx := bytes.IndexByte(msg, byte('\x02'))
+			if metadataEndIdx == -1 {
+				log.Println("Metadata seperator not found.")
+			}
+
+			var metadata fileMetadata
+			parseErr := json.Unmarshal(msg[:metadataEndIdx], &metadata)
+			if parseErr != nil {
+				log.Println("Metadata parse error.")
+			} else {
+				// TODO: ensure files dir, make variable?
+				writeErr := os.WriteFile("files/testfile", msg[metadataEndIdx+1:], 0644)
+				if writeErr != nil {
+					log.Println("File write error.")
+				}
+
+				fmt.Printf("File received, name: '%s' type: '%s\n'", metadata.Name, metadata.Type)
+
+				//escaped := html.EscapeString(parsed.Body)
+				//encodedMessage, _ := json.Marshal(map[string]string{"sender": c.name, "message": escaped})
+				//c.room.forward <- encodedMessage
+
 			}
 		}
-
 	}
 }
 
